d7fd8b5b1da37198ca545042d5374486de2b78e1
spretosg <r.spielhofer@bluewin.ch>
2024-12-17 10:07:49 +0100

    license update

--- dev/mod_mapping1_V2.R
+++ dev/mod_mapping1_V2.R
@@ -7,6 +7,21 @@
 #' @noRd
 #'
 #' @importFrom shiny NS tagList
+#' 
+# Copyright (C) 2024 Reto Spielhofer; Norwegian Institute for Nature Research (NINA)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
 mod_delphi_round1_ui <- function(id){
   ns <- NS(id)
   
